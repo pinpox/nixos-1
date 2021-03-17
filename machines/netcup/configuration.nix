6d52a11104a2419e0260e327ea9a09c8ff7183c6
MayNiklas <info@niklas-steffen.de>
2021-03-17 20:03:15 +0100

    added netcup

--- machines/netcup/configuration.nix
+++ machines/netcup/configuration.nix
@@ -1,17 +1,24 @@
-{ config, pkgs, lib, ... }: {
-  imports = [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix> ];
+{ config, pkgs, lib, modulesPath, ... }: {
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-    autoResize = true;
+  imports = [ "${modulesPath}/profiles/qemu-guest.nix" ];
+
+  config = {
+
+    fileSystems."/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+      autoResize = true;
+    };
+
+    boot.growPartition = true;
+    boot.kernelParams = [ "console=ttyS0" ];
+    boot.loader.grub.device = "/dev/vda";
+    boot.loader.timeout = 0;
+
+    mainUser = "nik";
+    mainUserHome = "${config.users.extraUsers.${config.mainUser}.home}";
+    nasIP = "192.168.5.10";
+
+    programs.ssh.startAgent = false;
   };
-
-  boot.growPartition = true;
-  boot.kernelParams = [ "console=ttyS0" ];
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.timeout = 0;
-
-  programs.ssh.startAgent = false;
-
 }
